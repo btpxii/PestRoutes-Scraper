2cfe33cdafd898912840ecbe72a58c70ec4d1ab8
btpxii <btpaul12@gmail.com>
2023-06-29 00:44:10 -0400

    adjusted discord post req body

--- alert.py
+++ alert.py
@@ -6,10 +6,11 @@ from datetime import datetime
 def discordWebhook(message, logger):
     data = {
         "username": "PestRoutes Monitor",
-        "avatar_url": "https://food.fnr.sndimg.com/content/dam/images/food/fullset/2022/02/16/0/FNM_030122-Homemade-Bagels_s4x3.jpg",
+        "avatar_url": "https://play-lh.googleusercontent.com/4iNHH0LNRGdjJSTT5XJUYPsiIYWDBbessRGVN3pTb5lWAhNOV6KwNe2GJp8IQ8TkpKc",
         "embeds": [{
             "title": message,
             "footer": {"text": f"GitHub: btpxii | [ {datetime.utcnow().strftime('%Y-%m-%d %H:%M:%S.%f')[:-3]} UTC ]"},
+            "color": 0x00af86
         }]
     }
     res = requests.post(WEBHOOK, data=json.dumps(data), headers={"Content-Type": "application/json"})
