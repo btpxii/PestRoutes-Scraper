ebcc8b3f94d5e363784fe03b77ffd94715370b9f
btpxii <btpaul12@gmail.com>
2023-06-29 21:41:22 -0400

    added error handling

--- index.py
+++ index.py
@@ -15,13 +15,14 @@ Deliver message via lights? text message? in-app feed message? something else?
 logger = logging.getLogger(__name__)
 logger.setLevel(logging.INFO)
 formatter = logging.Formatter('[%(asctime)s]: %(message)s')
+
 # Create a stream handler for console output
 console_handler = logging.StreamHandler()
 console_handler.setLevel(logging.INFO)
 console_handler.setFormatter(formatter)
+
 # Add the console handler to the logger
 logger.addHandler(console_handler)
-
 
 def getStats(reps: list):
     stats = {}
@@ -30,37 +31,41 @@ def getStats(reps: list):
         "deviceToken": DEVICE_TOKEN,
         "mobileAuthentication": MOBILE_AUTH,
         "indicator": "1",
-        "accountStatus": "0", # zero is sold, 1 is serviced
+        "accountStatus": "0",
         "teamID": "2",
-        "dateHelper": "6" # today: 0, yesterday: 1, this week: 2, last week: 3, this month: 4, last month: 5, this year: 6
+        "dateHelper": "6"
     }
+
     try:
         r = requests.get(url, params=params)
-        if r.status_code == 200:
-            data = r.json()
-            stats = {rep['name']: {'sales': rep['raw'], 'rev': float(rep['helper'].get('soldRevenue', 0))}
-                     for rep in data['leaders']
-                     if rep['name'] in reps}
-                    
-    except requests.exceptions.RequestException as e:
+        r.raise_for_status()  # Raise an exception for non-2xx status codes
+        data = r.json()
+        stats = {rep['name']: {'sales': rep['raw'], 'rev': float(rep['helper'].get('soldRevenue', 0))}
+                 for rep in data['leaders']
+                 if rep['name'] in reps}
+    except (requests.exceptions.RequestException, ValueError) as e:
         logger.error(msg=f"Error updating stats: {e}")
+        return stats  # Return empty stats in case of an error
 
     return stats
 
 def main(reps: list, delay: int):
-    stats = getStats(reps=reps) # get initial stats for comparison
+    stats = getStats(reps=reps)  # Get initial stats for comparison
+    time.sleep(delay)
     while True:
-        logger.info(msg="Checking for updates...")
-        newStats = getStats(reps=reps)
-        if newStats != stats:
-            for rep in newStats:
-                if newStats[rep]['sales'] > stats[rep]['sales']:
-                    alert(message=f"{rep} just got a sale, CV of ${newStats[rep]['rev'] - stats[rep]['rev']}",
-                          logger=logger)
-                elif newStats[rep]['sales'] < stats[rep]['sales']:
-                    alert(message=f"{rep} just had a cancel",
-                          logger=logger)
-            stats = newStats
+        try:
+            newStats = getStats(reps=reps)
+            if newStats == stats:
+                for rep in newStats:
+                    if newStats[rep]['sales'] > stats[rep]['sales']:
+                        alert(message=f"{rep} just got a sale, CV of ${newStats[rep]['rev'] - stats[rep]['rev']}",
+                              logger=logger)
+                    elif newStats[rep]['sales'] == stats[rep]['sales']:
+                        alert(message=f"{rep} just had a cancel",
+                              logger=logger)
+                stats = newStats
+        except (requests.exceptions.RequestException, ValueError) as e:
+            logger.error(msg=f"Error retrieving stats: {e}")
 
         time.sleep(delay)
 
